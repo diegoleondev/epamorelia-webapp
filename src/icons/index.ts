bc1038bce5606334b5b9fa4bb84638eb552ec592
diegoleondev <diegoleon.dev@gmail.com>
2024-02-13 05:39:19 -0600

    initial icons created

---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as IconCircleXFilled } from "./circle-x-filled";
+export { default as IconEye } from "./eye";
+export { default as IconEyeOff } from "./eye-off";
+export { default as IconPony } from "./pony";
