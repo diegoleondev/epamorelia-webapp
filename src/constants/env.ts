afed0d4e45b135abc936b0f48a27177cfa527479
diegoleondev <diegoleon.dev@gmail.com>
2024-02-13 12:01:32 -0600

    [bugfix] NEXT_PUBLIC variables are not send to client

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,5 @@
-const { NEXT_PUBLIC_API_URL, NEXT_PUBLIC_DISCORD_INVITE_URL } = process.env;
-
 export const ENV = {
-  API_URL: NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
-  DISCORD_INVITE_URL: NEXT_PUBLIC_DISCORD_INVITE_URL ?? "https://discord.com/",
+  API_URL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
+  DISCORD_INVITE_URL:
+    process.env.NEXT_PUBLIC_DISCORD_INVITE_URL ?? "https://discord.com/",
 };
