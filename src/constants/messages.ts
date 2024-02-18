ed9d584fcf291c77f3f8267ef5b96c95ad7a09fe
diegoleondev <diegoleon.dev@gmail.com>
2024-02-17 21:59:03 -0600

    created error messages constants

---
+++ src/constants/messages.ts
@@ -0,0 +1,21 @@
+const MESSAGES = {
+  PASSWORD: {
+    SHORT: "La contraseña debe tener al menos 6 caracteres",
+    LONG: "La contraseña debe tener como máximo 100 caracteres",
+    REQUIRED: "La contraseña es requerida",
+    UNKNOWN: "Error desconocido",
+  },
+  EMAIL: {
+    REQUIRED: "El email es requerido",
+    INVALID: "El email es inválido",
+    UNKNOWN: "Error desconocido",
+    FORMAT: "El email es inválido",
+  },
+  USERNAME: {
+    REQUIRED: "El nombre de usuario es requerido",
+    SHORT: "El nombre de usuario debe tener al menos 3 caracteres",
+    LONG: "El nombre de usuario debe tener como máximo 25 caracteres",
+  },
+};
+
+export default MESSAGES;
