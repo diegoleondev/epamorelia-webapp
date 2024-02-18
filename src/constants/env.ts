d2708d8f2ca9b48b60a3988b9fc3fb640ad47b76
diegoleondev <diegoleon.dev@gmail.com>
2024-02-17 21:56:30 -0600

    [refactor] export changed to export by default

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,7 @@
-export const ENV = {
+const ENV = {
   API_URL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
   DISCORD_INVITE_URL:
     process.env.NEXT_PUBLIC_DISCORD_INVITE_URL ?? "https://discord.com/",
 };
+
+export default ENV;
