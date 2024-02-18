20c1090c237f46a59b0aa90408bf5df000a158c0
diegoleondev <diegoleon.dev@gmail.com>
2024-02-18 17:25:17 -0600

    added unique messages

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -10,11 +10,13 @@ const MESSAGES = {
     INVALID: "El email es inválido",
     UNKNOWN: "Error desconocido",
     FORMAT: "El email es inválido",
+    UNIQUE: "El email ya está en uso",
   },
   USERNAME: {
     REQUIRED: "El nombre de usuario es requerido",
     SHORT: "El nombre de usuario debe tener al menos 3 caracteres",
     LONG: "El nombre de usuario debe tener como máximo 25 caracteres",
+    UNIQUE: "El nombre de usuario ya está en uso",
   },
 };
 
