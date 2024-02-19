59007ee9a4f1f59ba80880a7e8d798b2a6d3030c
diegoleondev <diegoleon.dev@gmail.com>
2024-02-18 21:08:24 -0600

    added async button and loader

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
+export { default as ButtonAsync } from "./buttons/async";
 export { default as ButtonLink } from "./buttons/link";
 export { default as Button } from "./buttons/normal";
 export { default as ButtonRedirect } from "./buttons/redirect";
 export { default as LayoutScreenAuth } from "./layouts/screen-auth/screen-auth";
 export { default as UtilHrO } from "./utils/hr/o/o";
+export { default as UtilLoader } from "./utils/loader/loader";
 export { default as UtilMessageError } from "./utils/messages/error";
