efa0c7796b256337a54bc8f516abf6273a21ae9d
diegoleondev <diegoleon.dev@gmail.com>
2024-02-18 19:36:03 -0600

    created index components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as ButtonLink } from "./buttons/link";
+export { default as Button } from "./buttons/normal";
+export { default as ButtonRedirect } from "./buttons/redirect";
+export { default as LayoutScreenAuth } from "./layouts/screen-auth/screen-auth";
+export { default as UtilHrO } from "./utils/hr/o/o";
+export { default as UtilMessageError } from "./utils/messages/error";
