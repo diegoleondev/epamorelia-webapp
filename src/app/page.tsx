5d92dee5ab649777509fc55b3bc82f269f18f4a7
diegoleondev <diegoleon.dev@gmail.com>
2024-02-19 08:39:52 -0600

    created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,42 @@
-import Image from "next/image";
+"use client";
+
+import { ButtonAsync, ButtonLink } from "@/components";
+import { ENV } from "@/constants";
+import { useAuth } from "@/hooks";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const auth = useAuth();
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <span className={styles.text}>
+        <i className={styles.hero_icon}>
+          <i>🐎</i>
+        </i>
+        <h1>Bienvenido a la versión beta cerrada de Tu Pony App</h1>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Estamos encantados de tenerte aquí. Te invitamos a compartir cualquier
+          error, detalle, sugerencia o comentario que puedas tener sobre la
+          aplicación en nuestra comunidad de Discord.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>
+          Tu retroalimentación es invaluable para nosotros y nos ayuda a mejorar
+          constantemente la experiencia de Tu Pony App.
+        </p>
+        <ButtonLink color="secondary" href={ENV.DISCORD_INVITE_URL}>
+          Ir a discord
+        </ButtonLink>
+      </span>
+      <section>
+        <h2>Configuración</h2>
+        <ButtonAsync
+          color="secondary"
+          onClick={async () => await auth.logOut().catch(console.error)}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          cerrar sesión
+        </ButtonAsync>
+      </section>
     </main>
   );
 }
