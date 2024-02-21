147dcf63d7cffabfec8f6dda638493fb68b9bdce
diegoleondev <diegoleon.dev@gmail.com>
2024-02-21 10:57:12 -0600

    added link to home

--- src/components/layouts/screen-auth/screen-auth.tsx
+++ src/components/layouts/screen-auth/screen-auth.tsx
@@ -1,4 +1,6 @@
+import { ROUTES } from "@/constants";
 import { IconPony } from "@/icons";
+import Link from "next/link";
 import styles from "./screen-auth.module.css";
 
 interface LayoutScreenAuthProps {
@@ -9,15 +11,17 @@ export default function LayoutScreenAuth(props: LayoutScreenAuthProps) {
   return (
     <article className={styles.article}>
       <header className={styles.header}>
-        <div className={styles.logo}>
-          <IconPony
-            style={{
-              color: "black",
-              width: "100%",
-              aspectRatio: "1/1",
-            }}
-          />
-        </div>
+        <Link href={ROUTES.HOME} replace prefetch scroll={false}>
+          <div className={styles.logo}>
+            <IconPony
+              style={{
+                color: "black",
+                width: "100%",
+                aspectRatio: "1/1",
+              }}
+            />
+          </div>
+        </Link>
       </header>
       <main className={styles.main}>
         <h2 className={styles.title}>Version Beta Cerrada</h2>
