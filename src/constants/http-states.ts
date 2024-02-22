77b2c468ea6bd817341a0b21f0c5970cc3aa208d
diegoleondev <diegoleon.dev@gmail.com>
2024-02-21 20:34:57 -0600

    created http state constans

---
+++ src/constants/http-states.ts
@@ -0,0 +1,12 @@
+const HTTP_STATES = {
+  OK: "20",
+  CREATED: "201",
+  BAD_REQUEST: "400",
+  UNAUTHORIZED: "401",
+  FORBIDDEN: "403",
+  NOT_FOUND: "404",
+  CONFLICT: "409",
+  UNKNOWN_ERROR: "500",
+};
+
+export default HTTP_STATES;
