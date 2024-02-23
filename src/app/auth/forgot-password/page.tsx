126c9f6718dcc013e264f855e88c748f0b182fd4
diegoleondev <diegoleon.dev@gmail.com>
2024-02-23 13:52:15 -0600

    created forgot-password page

---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import { LayoutScreenAuth } from "@/components";
+import ForgotPasswordForm from "./form";
+
+export default function ForgotPasswordPassword() {
+  return (
+    <LayoutScreenAuth>
+      <ForgotPasswordForm />
+    </LayoutScreenAuth>
+  );
+}
