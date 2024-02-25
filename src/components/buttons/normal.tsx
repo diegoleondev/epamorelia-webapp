79036ef63ebadc627fa6545825eb074571567eae
diegoleondev <diegoleon.dev@gmail.com>
2024-02-25 17:01:12 -0600

    refactored button logic attributes

--- src/components/buttons/normal.tsx
+++ src/components/buttons/normal.tsx
@@ -1,10 +1,6 @@
 import type { ButtonProps } from "./types";
-import useButtonStyle from "./use-button-style";
+import useButtonStyle from "./button";
 
 export default function Button(props: ButtonProps) {
-  return (
-    <button {...props} {...useButtonStyle(props)}>
-      {props.children}
-    </button>
-  );
+  return <button {...useButtonStyle(props)}>{props.children}</button>;
 }
