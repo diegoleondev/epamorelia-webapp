2ec4894e7b6eab6aa532aa1203847a054a00fcdc
diegoleondev <diegoleon.dev@gmail.com>
2024-02-25 17:34:02 -0600

    changed main icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -4,4 +4,4 @@ export { default as IconCircleXFilled } from "./circle-x-filled";
 export { default as IconEye } from "./eye";
 export { default as IconEyeOff } from "./eye-off";
 export { default as IconInfoCircleFilled } from "./info-circle-filled";
-export { default as IconPony } from "./pony";
+export { default as IconButterfly } from "./butterfly";
