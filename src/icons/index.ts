e5ae84a3f6a28737bcf3d81cadc537cc3d02a5e2
diegoleondev <diegoleon.dev@gmail.com>
2024-02-24 22:40:11 -0600

    added icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,5 @@
+export { default as IconAdjustmentsFilled } from "./adjustments-filled";
+export { default as IconArrowLeft } from "./arrow-left";
 export { default as IconCircleXFilled } from "./circle-x-filled";
 export { default as IconEye } from "./eye";
 export { default as IconEyeOff } from "./eye-off";
