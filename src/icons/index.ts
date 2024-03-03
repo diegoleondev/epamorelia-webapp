71b420ac5f7d41e041cfd2bbc564fa6980c3432d
diegoleondev <diegoleon.dev@gmail.com>
2024-03-02 23:55:26 -0600

    added icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,7 +1,10 @@
 export { default as IconAdjustmentsFilled } from "./adjustments-filled";
 export { default as IconArrowLeft } from "./arrow-left";
+export { default as IconButterfly } from "./butterfly";
+export { default as IconChevronRight } from "./chevron-right.tsx";
 export { default as IconCircleXFilled } from "./circle-x-filled";
 export { default as IconEye } from "./eye";
 export { default as IconEyeOff } from "./eye-off";
 export { default as IconInfoCircleFilled } from "./info-circle-filled";
-export { default as IconButterfly } from "./butterfly";
+export { default as IconPlus } from "./plus";
+export { default as IconUser } from "./user";
---
+++ src/icons/user.tsx
@@ -0,0 +1,20 @@
+import icon from "./icon";
+import { type IconProps } from "./types";
+
+export default function IconUser(props: IconProps) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...icon(props)}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+      <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+    </svg>
+  );
+}
