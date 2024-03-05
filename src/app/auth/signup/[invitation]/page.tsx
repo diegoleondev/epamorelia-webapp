ab226d401dedac12975da0178efcc5b9823aa5cc
diegoleondev <diegoleon.dev@gmail.com>
2024-03-05 11:40:12 -0600

    [refactor] new logic of find invigation

--- src/app/auth/signup/[invitation]/page.tsx
+++ src/app/auth/signup/[invitation]/page.tsx
@@ -1,26 +1,33 @@
-import {
-  LayoutScreenAuth,
-  Text,
-  TextParagraph,
-  TextSkeleton,
-} from "@/components";
-import { Suspense } from "react";
+import { findOneUserInvitationApi } from "@/api/user-invitation";
+import { LayoutScreenAuth, Text, TextParagraph } from "@/components";
+import Paragraph from "@/components/texts/paragraph";
+import { ROUTES } from "@/constants";
+import { redirect } from "next/navigation";
 import SignUpForm from "./form";
-import SourceUsername from "./sourceUsername";
+
+export const dynamic = "force-dynamic";
 
 export default async function Page({
   params,
 }: {
   params: { invitation: string };
 }) {
+  const result = await findOneUserInvitationApi({ id: params.invitation });
+
+  if (!result.success) {
+    return redirect(ROUTES.SIGN_UP_ERROR);
+  }
+
+  console.log(result);
   return (
     <LayoutScreenAuth>
       <Text flex={["column", "ai-center"]}>
-        <Suspense fallback={<TextSkeleton width="5em" height="large" />}>
-          <SourceUsername invitation={params.invitation} />
-        </Suspense>
+        <Paragraph size="large" weight="bold">
+          {result.data.sourceUserName}
+        </Paragraph>
         <TextParagraph align="center">
-          Te ha invitado a unirte a la sede $branch$
+          Te ha invitado a unirte a la sede {result.data.branchName} como{" "}
+          {result.data.roleId}
         </TextParagraph>
       </Text>
       <SignUpForm invitation={params.invitation} />
