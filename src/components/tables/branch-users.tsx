ec1350e6e597a7f6bb312dea313fa3c00828a868
diegoleondev <diegoleon.dev@gmail.com>
2024-03-31 13:08:18 -0600

    [style] refactor table style

--- src/components/tables/branch-users.tsx
+++ src/components/tables/branch-users.tsx
@@ -2,51 +2,46 @@
 
 import { findAllBranchUsersAPI } from "@/api/branch";
 import { ROUTES } from "@/constants";
+import parseClassNames from "@/utils/parseClassNames";
 import { useEffect, useState } from "react";
-import { Anchor, Text } from "..";
+import { Anchor, Text, UtilLoader } from "..";
 import styles from "./table.module.css";
 
 export default function BranchUsers(props: { branchId: string }) {
-  const [data, setData] = useState<User[] | null>(null);
-  const [error, setError] = useState("");
+  const [data, setData] = useState<User[] | undefined>(undefined);
 
   useEffect(() => {
     findAllBranchUsersAPI({ id: props.branchId })
       .then((response) => {
         if (response.success) {
           setData(response.data);
-          setError("");
-          return;
         }
-
-        setError(response.details._ ?? "");
       })
       .catch(() => {});
   }, []);
 
-  if (error !== "") {
-    return <div>Algo salio mal</div>;
-  }
-
-  if (data === undefined || data === null) {
-    return <div>No hay usuarios</div>;
-  }
-
   return (
-    <section className={`${styles.table} ${styles.tableBranchUsers}`}>
-      <div className={styles.row}>
-        <Text>Nombre</Text>
-        <Text>Rol</Text>
-        <Text>email</Text>
+    <section className={parseClassNames(styles.table, styles.formUserData)}>
+      <div className={parseClassNames(styles.row, styles.header)}>
+        <Text className={styles.cell}>Nombre</Text>
+        <Text className={styles.cell}>Rol</Text>
+        <Text className={styles.cell}>email</Text>
       </div>
-      {data.map((branch) => (
-        <Anchor key={branch.id} href={`${ROUTES.BRANCH}/${branch.id}`}>
-          <Text>{branch.username}</Text>
-          <Text>{branch.roleId}</Text>
-          <Text>{branch.email}</Text>
-        </Anchor>
-      ))}
-      <Anchor className={styles.row} href={`${ROUTES.USER}/new`}>
+      {data === undefined && (
+        <div className={styles.empty}>
+          <UtilLoader />
+        </div>
+      )}
+      <div className={styles.body}>
+        {data?.map((branch) => (
+          <div key={branch.id} className={styles.row}>
+            <Text className={styles.cell}>{branch.username}</Text>
+            <Text className={styles.cell}>{branch.roleId}</Text>
+            <Text className={styles.cell}>{branch.email}</Text>
+          </div>
+        ))}
+      </div>
+      <Anchor className={styles.footer} href={`${ROUTES.USER}/new`}>
         Invitar usuario
       </Anchor>
     </section>
