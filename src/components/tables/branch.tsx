0a2a40c1c967b986ded93f2b9252f8893e59696b
diegoleondev <diegoleon.dev@gmail.com>
2024-04-01 14:06:01 -0600

    [style] refactored styles

--- src/components/tables/branch.tsx
+++ src/components/tables/branch.tsx
@@ -1,28 +1,109 @@
-import { findAllBranchesAPI } from "@/api/branch";
-import { Anchor, Text } from "@/components";
+"use client";
+
+import {
+  Button,
+  ButtonLink,
+  Modal,
+  Text,
+  Title,
+  UtilLoader,
+} from "@/components";
 import { ROUTES } from "@/constants";
+import parseClassNames from "@/utils/parseClassNames";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import styles from "./table.module.css";
 
-export default async function Table() {
-  const { data } = await findAllBranchesAPI();
+interface BranchTableProps {
+  data: Branch[];
+}
+
+interface ActionsProps {
+  selected: string;
+  onClose: () => void;
+  data: Branch[];
+}
+
+function ModalActions(props: ActionsProps) {
+  const [branch, setBranch] = useState<Branch | undefined>(undefined);
+
+  useEffect(() => {
+    const branch = props.data.find((branch) => branch.id === props.selected);
+
+    if (branch !== undefined) {
+      setBranch(branch);
+    }
+  }, [props.data]);
 
   return (
-    <section className={styles.table}>
-      <div className={styles.row}>
-        <Text>Sede</Text>
-        <Text>Limite</Text>
-        <Text>Registrados</Text>
-      </div>
-      {data?.map((branch) => (
-        <Anchor key={branch.id} href={`${ROUTES.BRANCH}/${branch.id}`}>
-          <Text>{branch.name}</Text>
-          <Text>{branch.limit}</Text>
-          <Text>0</Text>
-        </Anchor>
-      ))}
-      <Anchor className={styles.row} href={`${ROUTES.BRANCH}/new `}>
-        Agregar nueva sede
-      </Anchor>
-    </section>
+    <>
+      <Title>{branch?.name}</Title>
+      <ButtonLink href={`${ROUTES.BRANCH}/${branch?.id}`}>Ver</ButtonLink>
+      <Button onClick={() => toast.success("En desarrollo 😊")}>
+        Descargar excel
+      </Button>
+      <Button onClick={props.onClose} color="secondary">
+        Cerrar
+      </Button>
+    </>
+  );
+}
+
+export default function Table(props: BranchTableProps) {
+  const { data } = props;
+  const [modal, setModal] = useState<string>("");
+
+  const handleAction = (event: any) => {
+    const target = event.target as HTMLElement;
+    const id = target.getAttribute("data-id");
+
+    if (typeof id === "string") {
+      console.log(id);
+      setModal(id);
+    }
+  };
+
+  const closeModal = () => {
+    setModal("");
+  };
+
+  return (
+    <>
+      <section className={parseClassNames(styles.table)}>
+        <div className={parseClassNames(styles.row, styles.header)}>
+          <Text className={styles.cell}>Sede</Text>
+          <Text className={styles.cell} align="center">
+            Limite
+          </Text>
+          <Text className={styles.cell} align="center">
+            Registrados
+          </Text>
+        </div>
+        {data === undefined && (
+          <div className={styles.empty}>
+            <UtilLoader />
+          </div>
+        )}
+        <div className={styles.body} onClick={handleAction}>
+          {data.map((branch) => (
+            <div key={branch.id} data-id={branch.id} className={styles.row}>
+              <Text className={styles.cell}>{branch.name}</Text>
+              <Text className={styles.cell} align="center">
+                {branch.limit}
+              </Text>
+              <Text className={styles.cell} align="center">
+                {branch.counter}
+              </Text>
+            </div>
+          ))}
+        </div>
+        <ButtonLink className={styles.footer} href={`${ROUTES.BRANCH}/new `}>
+          Agregar nueva sede
+        </ButtonLink>
+      </section>
+      <Modal open={modal !== ""} onClose={closeModal}>
+        <ModalActions data={data} selected={modal} onClose={closeModal} />
+      </Modal>
+    </>
   );
 }
