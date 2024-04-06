4eb2bcb8dae5793947f1b3399a7df8ee50d88e54
diegoleondev <diegoleon.dev@gmail.com>
2024-04-06 00:01:07 -0600

    implemented conditional menu

--- src/components/forms/user-invitation.tsx
+++ src/components/forms/user-invitation.tsx
@@ -2,13 +2,15 @@
 
 import { findAllBranchesAPI } from "@/api/branch";
 import { createUserInvitationApi } from "@/api/user-invitation";
-import { ButtonAsync, ButtonLink, UtilMessageError } from "@/components";
-import { ENV, MESSAGES, ROUTES } from "@/constants";
+import { ButtonAsync, ModalShareLink, UtilMessageError } from "@/components";
+import { ENV, HTTP_STATES, MESSAGES, ROUTES } from "@/constants";
+import ROLES from "@/constants/roles";
 import useUserContext from "@/contexts/user";
 import { detailsToMessage } from "@/utils/details-to-message";
 import { type Details } from "@/validators/validatorHandler";
 import { useEffect, useRef, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import Paragraph from "../texts/paragraph";
 
 interface Props {
   initialData?: {
@@ -19,20 +21,17 @@ interface Props {
   };
 }
 
-const getDataForm = (form: HTMLFormElement) => {
-  const formData = new FormData(form);
-  const data = {
-    roleId: String(formData.get("roleId")) as "ADMIN",
-    branchId: String(formData.get("branchId")),
-    reference: String(formData.get("reference")),
-  };
+interface SubmitInvitation {
+  roleId: string;
+  branchId: string;
+  reference: string;
+}
 
-  return data;
-};
-
-const submitHandler = async (formRef: HTMLFormElement) => {
-  const form = getDataForm(formRef);
-
+const submitHandler = async (
+  form: Partial<SubmitInvitation>,
+  formRef: HTMLFormElement,
+) => {
+  // @ts-expect-error data is not defined
   const { details, success, data } = await createUserInvitationApi(form);
 
   if (success) {
@@ -55,24 +54,10 @@ const submitHandler = async (formRef: HTMLFormElement) => {
   };
 };
 
-/* const updateHandler = async (formRef: HTMLFormElement, branchId: string) => {
-  const data = getDataForm(formRef);
-
-  const { details, success } = await updateBranchApi({ ...data, id: branchId });
-
-  if (success) {
-    formRef.reset();
-    toast.success(`Sede ${data.name} actualizada`);
-    return;
-  }
-
-  const messages = detailsToMessage({
-    dictionary: MESSAGES.BRANCH,
-    details,
-  });
-
-  return messages;
-}; */
+const getRoles = (weight: number) =>
+  Object.entries(ROLES)
+    .filter(([, role]) => role < weight)
+    .map(([key]) => key);
 
 export default function FormUser(props: Props) {
   const user = useUserContext();
@@ -80,12 +65,26 @@ export default function FormUser(props: Props) {
   const formRef = useRef<HTMLFormElement>(null);
   const [allBranches, setAllBranches] = useState<Branch[]>([]);
   const [errors, setErrors] = useState<Details>({});
+  const [form, setForm] = useState({
+    roleId: "STAFF",
+    branchId: props.initialData?.branchId ?? user.branchId,
+    reference: "",
+  });
 
   const [invitationId, setInvitationId] = useState<string | null>(null);
 
+  const onUpdate = (event: any) => {
+    const { name, value } = event.target;
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   const createUserInvitation = async () => {
     if (formRef.current === null) return;
-    const response = await submitHandler(formRef.current);
+    const response = await submitHandler(form, formRef.current);
     setErrors(response.messages);
     setInvitationId(response.data);
   };
@@ -100,6 +99,10 @@ export default function FormUser(props: Props) {
       .catch(() => {});
   }, []);
 
+  if (errors._ === HTTP_STATES.UNAUTHORIZED) {
+    return <Paragraph>Acceso no autorizado</Paragraph>;
+  }
+
   return (
     <>
       <form
@@ -111,45 +114,60 @@ export default function FormUser(props: Props) {
       >
         <label className="label">
           Rol
-          <select className="input" name="roleId">
-            <option value="ADMIN">ADMIN</option>
-            <option value="MANAGER">MANAGER</option>
-            <option value="STAFF">STAFF</option>
-            <option value="USER">USER</option>
-          </select>
-          <UtilMessageError>{errors.roleId}</UtilMessageError>
-        </label>
-        <label className="label">
-          Sede
           <select
             className="input"
-            name="branchId"
-            defaultValue={user.branchId}
+            name="roleId"
+            value={form.roleId}
+            onChange={onUpdate}
           >
-            {allBranches.map((branch) => (
-              <option key={branch.id} value={branch.id}>
-                {branch.name}
+            {getRoles(user.role).map((value) => (
+              <option key={value} value={value}>
+                {value}
               </option>
             ))}
           </select>
-          <UtilMessageError>{errors.branchId}</UtilMessageError>
+          <UtilMessageError>{errors.roleId}</UtilMessageError>
         </label>
+        {user.role >= ROLES.ADMIN && (
+          <label className="label">
+            Sede
+            <select
+              className="input"
+              name="branchId"
+              defaultValue={user.branchId}
+              onChange={onUpdate}
+              value={form.branchId}
+            >
+              {allBranches.map((branch) => (
+                <option key={branch.id} value={branch.id}>
+                  {branch.name}
+                </option>
+              ))}
+            </select>
+            <UtilMessageError>{errors.branchId}</UtilMessageError>
+          </label>
+        )}
         <label>
           Referencia
-          <input type="text" name="reference" className="input" />
+          <input
+            type="text"
+            name="reference"
+            className="input"
+            value={form.reference}
+            onChange={onUpdate}
+          />
           <UtilMessageError>{errors.reference}</UtilMessageError>
         </label>
         <ButtonAsync onClick={createUserInvitation}>Crear</ButtonAsync>
         <UtilMessageError>{errors._}</UtilMessageError>
       </form>
-      {invitationId !== null && (
-        <ButtonLink
-          href={`https://api.whatsapp.com/send?text=Hola, te invito a EPA Morelia, por favor registrate en la siguiente ${ENV.APP_URL}/${ROUTES.SIGN_UP}/${invitationId}`}
-          target="_blank"
-        >
-          Enviar invitation por WhatsApp
-        </ButtonLink>
-      )}
+      <ModalShareLink
+        isOpen={invitationId !== null}
+        onClose={() => {
+          setInvitationId(null);
+        }}
+        url={`${ENV.APP_URL}${ROUTES.SIGN_UP}/${invitationId}`}
+      />
       <Toaster />
     </>
   );
