d316235561ec08e922283d58a9bbca7ab05301dc
diegoleondev <diegoleon.dev@gmail.com>
2024-05-11 10:57:10 -0600

    added timestamp parser

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -17,3 +17,16 @@ export function index<T extends Record<string, any>>(
     return acc;
   }, {});
 }
+
+export function timestamp() {
+  const date = new Date();
+
+  const year = date.getFullYear().toString().slice(2, 4);
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  return year + month + day + " " + hours + minutes + seconds;
+}
